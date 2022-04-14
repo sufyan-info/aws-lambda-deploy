5abee682d9b009661952445e4ce3455ba7d22de2
Abu Sufyan <liam.logan.web@gmail.com>
2022-04-14 17:03:01 +0600

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -4,5 +4,5 @@
 # exec.sh 1.3
 
 docker build -t aws-lambda-deploy:$1 .
-docker tag react-base:$1 super37/aws-lambda-deploy:$1
+docker tag aws-lambda-deploy:$1 super37/aws-lambda-deploy:$1
 docker push super37/aws-lambda-deploy:$1
